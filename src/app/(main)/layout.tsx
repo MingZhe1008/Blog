c39a2c23b2f3dbf2b4c6e8b9e6c3934c71cc3cca
MingZhe <zhangmingzhe@yonyou.com>
2026-09-16 16:46:46 +0800

    style: 按参考图调整白边黑色蓝光鼠标指针

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,13 +1,11 @@
 import { Header } from "@/components/header";
 import { StarsCanvas } from "@/components/star-background";
 import { SiteFooter } from "@/components/site-footer";
-import { PointerHighlight } from "@/components/pointer-highlight";
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="public-site">
       <StarsCanvas />
-      <PointerHighlight />
       <Header />
       <div className="public-site__content">{children}<SiteFooter /></div>
     </div>
